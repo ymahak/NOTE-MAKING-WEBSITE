cd7bd85e32851cbd659d5b80a83ee6e741bb0f4d
mahak <118602944+ymahak@users.noreply.github.com>
2023-02-24 17:43:24 +0530

    Add files via upload

---
+++ mahak project/src/components/App.jsx
@@ -0,0 +1,16 @@
+import Heading from "./Heading";
+import Footer from "./Footer";
+import Note from "./Note";
+const react = require("react");
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Footer />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
---
+++ mahak project/src/components/Note.jsx
@@ -0,0 +1,12 @@
+const react = require("react");
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>DevTown</h1>
+      <p>A place to learn and grow on your own pace</p>
+    </div>
+  );
+}
+
+export default Note;
